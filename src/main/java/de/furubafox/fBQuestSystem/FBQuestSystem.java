cfb85092dfa42210b1552a20406b6cf780642714
FurubaFox <68753584+ThisNightFox@users.noreply.github.com>
2024-08-27 22:17:38 +0200

    Edit Code structure

--- src/main/java/de/furubafox/fBQuestSystem/FBQuestSystem.java
+++ src/main/java/de/furubafox/fBQuestSystem/FBQuestSystem.java
@@ -1,5 +1,6 @@
 package de.furubafox.fBQuestSystem;
 
+import de.furubafox.fBQuestSystem.commands.AdminQuestGui;
 import de.furubafox.fBQuestSystem.commands.PlayerQuestGui;
 import de.furubafox.fBQuestSystem.utils.Util;
 import dev.dejvokep.boostedyaml.YamlDocument;
@@ -8,12 +9,10 @@ import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
 import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
 import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
 import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
-import me.clip.placeholderapi.PlaceholderAPIPlugin;
 import org.black_ixx.playerpoints.PlayerPoints;
 import org.black_ixx.playerpoints.PlayerPointsAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -27,15 +26,14 @@ import java.util.logging.Logger;
 public final class FBQuestSystem extends JavaPlugin implements Listener {
     public static final String prefix = Util.Color("&f[&6FB-&aQuestsystem&f] §8₪");
     private static FBQuestSystem instance;
-    PlayerPointsAPI ppAPI = PlayerPoints.getInstance().getAPI();
     private YamlDocument mainConfig;
     private YamlDocument messages;
 
     @Override
     public void onEnable() {
         Logger log = getLogger();
-
-        log.info("QuestPlugin is enabled!");
+        sendMessage("§aEnabled");
+        sendMessage2("Activated");
 
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
             /*
@@ -52,10 +50,10 @@ public final class FBQuestSystem extends JavaPlugin implements Listener {
             log.info("=============================================");
             getServer().getPluginManager().disablePlugin(this);
             return;
-        }log.info("Es wurde das Plugin PlaceholderAPI gefunden." + PlaceholderAPIPlugin.getServerVersion().getVersion());
+        }
 
         if (Bukkit.getPluginManager().isPluginEnabled("PlayerPoints")) {
-            log.info("Playerpoints Plugin found!");
+            PlayerPointsAPI ppAPI = PlayerPoints.getInstance().getAPI();
         } else {
             log.info("=============================================");
             log.info(" ");
@@ -64,6 +62,7 @@ public final class FBQuestSystem extends JavaPlugin implements Listener {
             log.info(" ");
             log.info("=============================================");
             getServer().getPluginManager().disablePlugin(this);
+            return;
         }
         try {
             this.mainConfig = YamlDocument.create(new File(getDataFolder(), "config.yml"),
@@ -76,9 +75,21 @@ public final class FBQuestSystem extends JavaPlugin implements Listener {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        try {
+            this.messages = YamlDocument.create(new File(getDataFolder(), "languages/messages.yml"),
+                    Objects.requireNonNull(getResource("languages/messages.yml")),
+                    GeneralSettings.builder().setUseDefaults(false).build(),
+                    LoaderSettings.builder().setAutoUpdate(true).build(),
+                    DumperSettings.DEFAULT,
+                    UpdaterSettings.builder().setVersioning(new BasicVersioning("config-version")).build()
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         //Commands registrieren
         loadListener(Bukkit.getPluginManager());
-        Objects.requireNonNull(getCommand("tpo")).setExecutor((CommandExecutor) new PlayerQuestGui(this.mainConfig,this.messages));
+        Objects.requireNonNull(getCommand("fbquest")).setExecutor(new PlayerQuestGui(this.mainConfig,this.messages));
+        Objects.requireNonNull(getCommand("fbquestadmin")).setExecutor(new AdminQuestGui(this.mainConfig,this.messages));
 
     }
 
@@ -90,14 +101,21 @@ public final class FBQuestSystem extends JavaPlugin implements Listener {
 
     @Override
     public void onDisable() {
-        getLogger().info("QuestPlugin is disabled!");
+        sendMessage("§4Deaktiviert");
     }
 
+    private void sendMessage2(String enabled) {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "=====------===== " + prefix + ChatColor.AQUA +"=====------=====");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "§f[§6FB-§aQuest§8System§f] " + ChatColor.BLUE + "Placeholder: " + ChatColor.GREEN + enabled );
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "§f[§6FB-§aQuest§8System§f] " + ChatColor.BLUE + "Playerpoints: " + ChatColor.GREEN + enabled);
+        // Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "§f[§6FB-§aQuest§8System§f] " + ChatColor.BLUE + "Citiziens: " + ChatColor.GREEN + enabled);
+        Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "=====------===== " + prefix + ChatColor.AQUA +"=====------=====");
+    }
     private void sendMessage(String loading) {
         Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "=====------===== " + prefix + ChatColor.AQUA +"=====------=====");
-        Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "§f[§6FB-§aMulti§8System§f] " + ChatColor.BLUE + "Status: " + ChatColor.GREEN + loading );
-        Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "§f[§6FB-§aMulti§8System§f] " + ChatColor.BLUE + "Author: " + this.getDescription().getAuthors());
-        Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "§f[§6FB-§aMulti§8System§f] " + ChatColor.BLUE + "Version: " + this.getDescription().getVersion());
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "§f[§6FB-§aQuest§8System§f] " + ChatColor.BLUE + "Status: " + ChatColor.GREEN + loading );
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "§f[§6FB-§aQuest§8System§f] " + ChatColor.BLUE + "Author: " + this.getDescription().getAuthors());
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "§f[§6FB-§aQuest§8System§f] " + ChatColor.BLUE + "Version: " + this.getDescription().getVersion());
         Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "=====------===== " + prefix + ChatColor.AQUA +"=====------=====");
     }
 
