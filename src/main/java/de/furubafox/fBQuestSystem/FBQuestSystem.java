372682f0faca080ffea4c6b23c078de44fcf9e94
FurubaFox <68753584+ThisNightFox@users.noreply.github.com>
2024-08-27 22:45:32 +0200

    Added new funktions

--- src/main/java/de/furubafox/fBQuestSystem/FBQuestSystem.java
+++ src/main/java/de/furubafox/fBQuestSystem/FBQuestSystem.java
@@ -2,6 +2,7 @@ package de.furubafox.fBQuestSystem;
 
 import de.furubafox.fBQuestSystem.commands.AdminQuestGui;
 import de.furubafox.fBQuestSystem.commands.PlayerQuestGui;
+import de.furubafox.fBQuestSystem.listeners.GuiClickListener;
 import de.furubafox.fBQuestSystem.utils.Util;
 import dev.dejvokep.boostedyaml.YamlDocument;
 import dev.dejvokep.boostedyaml.dvs.versioning.BasicVersioning;
@@ -95,8 +96,7 @@ public final class FBQuestSystem extends JavaPlugin implements Listener {
 
     //Listeners registrieren
     private void loadListener(final PluginManager pluginManager) {
-        //pluginManager.registerEvents(new PlayerQuestGui(this.mainConfig, this.messages),this);
-        pluginManager.registerEvents(this, this);
+        pluginManager.registerEvents(new GuiClickListener(),this);
     }
 
     @Override
