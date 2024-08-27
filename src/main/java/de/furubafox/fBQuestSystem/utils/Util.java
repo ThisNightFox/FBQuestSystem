e042f62c5a53030b1290943ae32f8908eda73626
FurubaFox <68753584+ThisNightFox@users.noreply.github.com>
2024-08-27 20:00:32 +0200

    remove error

--- src/main/java/de/furubafox/fBQuestSystem/utils/Util.java
+++ src/main/java/de/furubafox/fBQuestSystem/utils/Util.java
@@ -1,14 +1,9 @@
 package de.furubafox.fBQuestSystem.utils;
 
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.entity.Player;
-
-import java.util.HashMap;
 
 public class Util {
     public static String Color(String s) {
         return ChatColor.translateAlternateColorCodes('&', s);
     }
-
-    public static HashMap<Player, Player> tpas = new HashMap<Player, Player>();
 }
